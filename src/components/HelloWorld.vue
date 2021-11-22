b1e2e59a8b1fdc7d3a094ce0f468a6f457689d15
Noam Saoudi <311noam@MacBook-Pro-de-Noam.local>
2021-11-22 13:46:54 +0100

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,22 +1,29 @@
 <template>
 
-  <ul>
+   <h1>hello</h1>
+<ul v-if="show">
   <li v-for="item in items" :key="item.message">
     {{ item.message }}
   </li>
 </ul>
+ <button v-on:click="show = true">aficher</button>
+ <button v-on:click="show = false">cacher</button>
 
 </template>
 
 <script>
 export default {
   name: "HelloWorld",
-  data() {
+   props: {
+    msg: String
+},
+ data() {  
     return {
       items: [{ message: "Ellyes" }, { message: "Noam" }, { message: "Souri" }, { message: "Romain" }, { message: "Antho" }, ],
+      show :true
     };
-  },
-};
+  }
+}
 </script>
 
 <style scoped>
